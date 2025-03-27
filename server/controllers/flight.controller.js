f9d11872b6125e6b8bce97eac1e3c3d900ad5110
Punsano26 <xlichaphlreuxn@gmail.com>
2025-03-27 16:37:00 +0700

    start controller

---
+++ server/controllers/flight.controller.js
@@ -0,0 +1,10 @@
+const FlightModel = require("../models/flight");
+Model = require("../models/flight");
+require("dotenv").config();
+const secret = process.env.SECRET;
+
+exports.createFlight = async (res,req) => {
+    if(!user) {
+        
+    }
+}
